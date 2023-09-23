47cd4880e7ee89a99def7b9459ba07779fc393ef
鈴木響 <h.suzuki@piton.jp>
2023-09-23 19:31:50 +0900

    fronted

---
+++ fronted/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+
+    <h1>{{ msgFromLaravel }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msgFromLaravel: ''
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:8080/api/message')
+      .then(response => {
+        this.msgFromLaravel = response.data.msg;
+      })
+      .catch(error => {
+        console.error('There was an error fetching the message:', error);
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
